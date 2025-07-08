31c2068842158e621464d541a128a2f9ed7e6d1e
lzr224 <70804490+lzr224@users.noreply.github.com>
2025-07-08 18:27:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from endpoints import router as recipe_router
 from database import init_db
 
 app = FastAPI()
 
-# 初始化数据库并插入初始数据
 init_db()
-
-# 加载所有 API 路由
 app.include_router(recipe_router)
+
+# 如果本地执行 main.py，自动运行 uvicorn
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
