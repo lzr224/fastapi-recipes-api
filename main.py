ba246d1f7400e41c51e383b49a06c229f57035b6
lzr224 <70804490+lzr224@users.noreply.github.com>
2025-07-08 19:04:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
-import os
-import uvicorn
 from fastapi import FastAPI
-
-from endpoints import router as recipe_router
-from database import init_db
 
 app = FastAPI()
 
-# 根路径返回提示
 @app.get("/")
-def read_root():
-    return {"message": "FastAPI is running!"}
-
-# 初始化数据库
-init_db()
-
-# 注册路由
-app.include_router(recipe_router)
-
-# Railway 会自动设置 PORT 环境变量
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+def root():
+    return {"message": "Hello!"}
